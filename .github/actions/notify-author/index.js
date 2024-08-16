1eff347621bf8ebf1ca45ba100e3816c4323797e
Ingewar <kovtunihor@gmail.com>
2024-08-16 14:16:44 +0300

    add yaml support

--- .github/actions/notify-author/index.js
+++ .github/actions/notify-author/index.js
@@ -1,10 +1,14 @@
 // .github/actions/my-custom-action/index.js
 const core = require('@actions/core');
 const users = require('./users.json');
+const readYamlFile = require('read-yaml-file');
 
 try {
   const slackToken = core.getInput('slack-bot-token');
   const whoToNotify = core.getInput('who-to-notify');
+  const contributorsPath = core.getInput('contributors-path');
+  const contributors = readYamlFile.sync(contributorsPath);
+  console.log(contributors);
   const user = users[whoToNotify];
   console.log(`Notifing ${user}`);
     // Set the output value
