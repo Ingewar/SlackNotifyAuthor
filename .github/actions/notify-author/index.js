5fb8952fe36bacb46b0a67e431771326f10751c5
Ingewar <kovtunihor@gmail.com>
2024-08-16 12:30:32 +0300

    add new input

--- .github/actions/notify-author/index.js
+++ .github/actions/notify-author/index.js
@@ -3,7 +3,9 @@ const core = require('@actions/core');
 
 try {
   const slackToken = core.getInput('slack-bot-token');
-  console.log(`Token is, ${slackToken}!`);
+  const whoToNotify = core.getInput('who-to-notify');
+  
+  console.log(`Notifing ${whoToNotify}`);
 } catch (error) {
   core.setFailed(error.message);
 }
