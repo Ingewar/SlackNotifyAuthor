55ec971688336c2b5f1d97d70f02eec23d75cf2c
Ingewar <kovtunihor@gmail.com>
2024-08-16 14:27:55 +0300

    remove input for the github actor

--- .github/actions/notify-author/index.js
+++ .github/actions/notify-author/index.js
@@ -2,11 +2,10 @@ const core = require('@actions/core');
 const readYamlFile = require('read-yaml-file');
 
 try {
-  const slackToken = core.getInput('slack-bot-token');
-  const whoToNotify = core.getInput('who-to-notify');
+  const githubActor = process.env.GITHUB_ACTOR
   const contributorsPath = core.getInput('contributors-path');
   const contributors = readYamlFile.sync(contributorsPath);
-  const user = contributors[whoToNotify];
+  const user = contributors[githubActor];
   console.log(user);
     // Set the output value
   core.setOutput('notified-user', user);
