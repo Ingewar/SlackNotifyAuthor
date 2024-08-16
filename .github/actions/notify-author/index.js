abc657a752ab2f5b016562ff4c23fa0e874b23c7
Ingewar <kovtunihor@gmail.com>
2024-08-16 14:03:41 +0300

    add output

--- .github/actions/notify-author/index.js
+++ .github/actions/notify-author/index.js
@@ -1,11 +1,15 @@
 // .github/actions/my-custom-action/index.js
 const core = require('@actions/core');
+const users = require('./users.json');
 
 try {
   const slackToken = core.getInput('slack-bot-token');
   const whoToNotify = core.getInput('who-to-notify');
-  
-  console.log(`Notifing ${whoToNotify}`);
+  const user = users[whoToNotify];
+  console.log(`Notifing ${user}`);
+    // Set the output value
+  core.setOutput('notified-user', user);
+
 } catch (error) {
   core.setFailed(error.message);
 }
